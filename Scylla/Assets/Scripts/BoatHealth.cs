3a57f515f6c53c782065e500c2313e19f42ca5d7
DanteNardo <dcn5783@g.rit.edu>
2017-01-22 13:25:55 -0500

    Added a healthbar and texture to the booooooooooooat.

---
+++ Scylla/Assets/Scripts/BoatHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class BoatHealth : MonoBehaviour
+{
+    #region BoatHealth Member Variables
+    private float m_startScale;
+    public float m_maxHealth;
+    public float m_curHealth;
+    public GameObject m_healthBar;
+    #endregion
+
+    #region BoatHealth Methods
+    void Start()
+    {
+        m_startScale = m_healthBar.transform.localScale.x;
+        m_curHealth = m_maxHealth;
+    }
+    
+    void Update()
+    {
+        //m_curHealth = GetComponent<BoatAI>().currentHealth;
+        SetHealthBar(m_curHealth / m_maxHealth);
+    }
+
+    private void SetHealthBar(float health)
+    {
+        m_healthBar.transform.localScale = new Vector3(Mathf.Clamp(health, 0f, 1f) * m_startScale, 
+                                                       m_healthBar.transform.localScale.y, 
+                                                       m_healthBar.transform.localScale.z);
+    }
+    #endregion
+}
