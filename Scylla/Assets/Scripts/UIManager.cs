9e86c2e68d4efea2c937e4572b925ed6ee638556
cjb5670 <cjb5670@rit.edu>
2017-01-22 12:22:56 -0500

    Menu Navigation implemented and Thirio added to control menu

---
+++ Scylla/Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void LoadScene(string scene)
+	{
+		SceneManager.LoadSceneAsync(scene);
+	}
+}
