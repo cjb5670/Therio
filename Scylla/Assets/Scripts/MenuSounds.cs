a57a8b789ca673a08009d05624ee19edb8d5fb15
cjb5670 <cjb5670@rit.edu>
2017-01-22 14:38:50 -0500

    Added Menu Sounds BUG: CHATTER WILL REPEAT WHEN RETURNING TO MAIN MENU

---
+++ Scylla/Assets/Scripts/MenuSounds.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSounds : MonoBehaviour
+{
+	static bool AudioBegin = false;
+	private AudioSource source;
+
+	// Use this for initialization
+	void Awake()
+	{
+		source = GetComponent<AudioSource>();
+
+		if (!AudioBegin)
+		{
+			source.Play();
+			DontDestroyOnLoad(gameObject);
+			AudioBegin = true;
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (SceneManager.GetActiveScene().name == "Scylla")
+		{
+			source.Stop();
+			AudioBegin = false;
+		}
+	}
+}
