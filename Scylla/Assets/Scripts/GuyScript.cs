c901808963ef3b5d44dc016ebb725cfb342015e5
IGMAdmin <IGMAdmin@GDD-46>
2017-01-22 03:31:55 -0500

    Expanded on the boat spawn point when you press x the Marines will now jump and yell yup

--- Scylla/Assets/Scripts/GuyScript.cs
+++ Scylla/Assets/Scripts/GuyScript.cs
@@ -10,20 +10,30 @@ public class GuyScript : MonoBehaviour
     public float vol;
      
     public AudioClip[] DrowningClips;
+    public AudioClip[] YellingClips;
     public GameObject Monster; 
 
-    private AudioSource source; 
+    private AudioSource source;
+    private Rigidbody2D rig;
 
     // Use this for initialization
     void Start()
     {
         source = this.GetComponent<AudioSource>();
+        rig = GetComponent<Rigidbody2D>();
     }
 
     public void IsEaten()
     {
         Debug.Log("I got eaten");
         Destroy(this.gameObject);
+    }
+
+    private void playClip(AudioClip clip)
+    {
+        var getVol = vol * (float)(1 / Vector3.Distance(this.transform.position, Monster.transform.position));
+        source.pitch = Random.Range(0.8f, 1);
+        source.PlayOneShot(clip, getVol);
     }
 
     // Update is called once per frame
@@ -34,24 +44,26 @@ public class GuyScript : MonoBehaviour
 
         if(this.transform.position.y >= WaterLevel)
         {
-            var rig = this.GetComponent<Rigidbody2D>();
             rig.drag = NotInWaterDrag;
             joint.enabled = false;
         }
         else
         {
-            var rig = this.GetComponent<Rigidbody2D>();
             rig.drag = InWaterDrag;
             joint.enabled = true;
 
             if(!source.isPlaying)
             {
-                var getVol = vol * (float)(1/Vector3.Distance(this.transform.position, Monster.transform.position));
-                Debug.Log("getvol = "+getVol);
-                source.pitch = Random.Range(0.8f, 1);
-                var loc = Random.Range(0, DrowningClips.Length - 1);
-                source.PlayOneShot(DrowningClips[loc], getVol);
+                var loc = Random.Range(0, 4);
+                playClip(DrowningClips[loc]);
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            rig.AddForce(new Vector2(Random.Range(-10,10), 20));
+            var loc = Random.Range(0, 4);
+            playClip(YellingClips[loc]);
         }
 
     }
