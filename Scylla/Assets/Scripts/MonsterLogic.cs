0cbd5e98c04701242a5a85c7ed510ff1a693a41f
IGMAdmin <IGMAdmin@GDD-46>
2017-01-22 13:07:11 -0500

    Added a jump to the monster.

--- Scylla/Assets/Scripts/MonsterLogic.cs
+++ Scylla/Assets/Scripts/MonsterLogic.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class MonsterLogic : MonoBehaviour {
 
+    public TentacleBase[] AllMyArms;
+    public KeyCode KeyJump;
+    public float JumpPower; 
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void DoJump()
+    {
+
+        Vector3 inFront = (transform.localRotation * new Vector3(0, 1, 1)) * JumpPower;
+        Debug.Log(string.Format("magdir x:{0}, y:{1}", inFront.x, inFront.y));
+
+        foreach (var arms in AllMyArms)
+        {
+            arms.ReleaseFromWall(); 
+        }
+
+        var rig = this.GetComponent<Rigidbody2D>();
+        rig.AddForce(inFront);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyJump))
+        {
+            DoJump(); 
+        }
+    }
 }
